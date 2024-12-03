f8c1ee6eb0336b066f46ef6c66921a2562b650f8
Zack <zack@ukaton.com>
2024-12-02 16:46:57 -0800

    .

--- Assets/BrilliantSole/DevicePair/BS_DevicePair.cs
+++ Assets/BrilliantSole/DevicePair/BS_DevicePair.cs
@@ -2,5 +2,14 @@ using UnityEngine;
 
 public class BS_DevicePair
 {
-    
+    private static readonly BS_Logger Logger = BS_Logger.GetLogger("BS_DevicePair", BS_Logger.LogLevel.Log);
+
+    public static readonly BS_DevicePair Instance;
+    public bool IsInstance => Instance == this;
+
+    static BS_DevicePair()
+    {
+        Instance = new();
+        // FILL - add deviceManager listeners
+    }
 }
