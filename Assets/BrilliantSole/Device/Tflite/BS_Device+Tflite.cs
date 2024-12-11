2c613b8f8f01355b2d6a817097456fced56ca56e
Zack <zack@ukaton.com>
2024-12-10 21:44:49 -0800

    Update BS_Device+Tflite.cs

--- Assets/BrilliantSole/Device/Tflite/BS_Device+Tflite.cs
+++ Assets/BrilliantSole/Device/Tflite/BS_Device+Tflite.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 public partial class BS_Device
 {
@@ -7,12 +8,41 @@ public partial class BS_Device
     {
         Managers.Add(TfliteManager);
 
-        // FILL
+        TfliteManager.OnIsReady += onIsTfliteReady;
+        TfliteManager.OnInference += onTfliteInference;
+        TfliteManager.OnInferencingEnabled += onTfliteInferencingEnabled;
+        TfliteManager.OnClassification += onTfliteClassification;
     }
 
     public void SendTfliteModel(BS_TfliteModelMetadata tfliteModelMetadata)
     {
         TfliteManager.SendTfliteModel(tfliteModelMetadata, false);
         SendFile(tfliteModelMetadata);
+    }
+
+    public bool IsTfliteReady => TfliteManager.IsReady;
+    public event Action<BS_Device, bool> OnIsTfliteReady;
+    private void onIsTfliteReady(bool isTfliteReady)
+    {
+        OnIsTfliteReady?.Invoke(this, isTfliteReady);
+    }
+
+    public bool TfliteInferencingEnabled => TfliteManager.InferencingEnabled;
+    public event Action<BS_Device, bool> OnTfliteInferencingEnabled;
+    private void onTfliteInferencingEnabled(bool inferencingEnabled)
+    {
+        OnTfliteInferencingEnabled?.Invoke(this, inferencingEnabled);
+    }
+
+    public event Action<BS_Device, List<float>, Dictionary<string, float>, ulong> OnTfliteInference;
+    private void onTfliteInference(List<float> inference, Dictionary<string, float> inferenceMap, ulong timestamp)
+    {
+        OnTfliteInference?.Invoke(this, inference, inferenceMap, timestamp);
+    }
+
+    public event Action<BS_Device, string, float, ulong> OnTfliteClassification;
+    private void onTfliteClassification(string className, float classValue, ulong timestamp)
+    {
+        OnTfliteClassification?.Invoke(this, className, classValue, timestamp);
     }
 }
