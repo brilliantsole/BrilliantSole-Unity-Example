b95441099ec92f734d139ab7a752787425778be0
Zack <zack@ukaton.com>
2024-12-28 11:30:06 -0800

    .

--- Assets/Hue/Editor/HueBridgeEditor.cs
+++ Assets/Hue/Editor/HueBridgeEditor.cs
@@ -1,23 +1,26 @@
 ﻿using UnityEditor;
 using UnityEngine;
-using System.Collections;
 
 [CustomEditor(typeof(HueBridge))]
-public class HueBridgeEditor : Editor {
+public class HueBridgeEditor : Editor
+{
 
-	public override void OnInspectorGUI() {
+	public override void OnInspectorGUI()
+	{
 		HueBridge hueBridge = (HueBridge)target;
 
-		hueBridge.hostName = EditorGUILayout.TextField ("Host name:", hueBridge.hostName);
-	//	hueBridge.portNumber = EditorGUILayout.IntField ("Port number:", hueBridge.portNumber);
-		hueBridge.username = EditorGUILayout.TextField ("Username:", hueBridge.username);
+		hueBridge.hostName = EditorGUILayout.TextField("Host name:", hueBridge.hostName);
+		//	hueBridge.portNumber = EditorGUILayout.IntField ("Port number:", hueBridge.portNumber);
+		hueBridge.username = EditorGUILayout.TextField("Username:", hueBridge.username);
 
-		if (GUILayout.Button ("Discover Lights")) {
+		if (GUILayout.Button("Discover Lights"))
+		{
 			hueBridge.DiscoverLights();
 		}
 
-		if (GUI.changed) {
-			EditorUtility.SetDirty (target);
+		if (GUI.changed)
+		{
+			EditorUtility.SetDirty(target);
 		}
 	}
 
